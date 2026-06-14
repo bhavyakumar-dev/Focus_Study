683c668a7c392d499545cc3c88256006075eb39c
bhavyakumar-dev <bhavyakumar6840@gmail.com>
2026-06-14 18:25:33 +0530

    fix: enable scrolling in PDF viewer by removing scrollbar=0 parameter

--- src/PdfViewer.jsx
+++ src/PdfViewer.jsx
@@ -2,7 +2,7 @@ function PdfViewer({ pdfUrl }) {
   return (
     <div style={{ width: '100%', height: '100%', backgroundColor: '#fff' }}>
       <iframe 
-        src={`${pdfUrl}#toolbar=0&navpanes=0&scrollbar=0`} 
+        src={`${pdfUrl}#toolbar=0&navpanes=0`} 
         width="100%" 
         height="100%" 
         style={{ border: 'none' }}
